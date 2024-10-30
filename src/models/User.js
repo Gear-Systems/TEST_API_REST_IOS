dd373e743cc593616374f8474bd6509fb4020a7f
Michel Cordova <ios.programacion4@gmail.com>
2024-10-30 16:24:04 -0600

    fourth commit

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../utils/connection');
+
+const User = sequelize.define('user', {
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    birthday: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        unique: true
+    },
+});
+
+module.exports = User;
